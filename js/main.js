168bfdb3a18db44d6fcaa82e9997ee0c3602ade8
ksugawara61 <katsuya@kokohadoko.net>
2018-08-15 12:58:42 +0000

    add: add countUp method

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 (function() {
-    'use strict':
+    'use strict';
 
     // Components
 
+    var likeComponent = Vue.extend({
+        data: function() {
+            return {
+                count: 0
+            }
+        },
+        template: '<button @click="countUp">Like {{ count }}</button>',
+        methods: {
+            countUp: function() {
+                this.count++;
+            }
+        }
+    });
+
     var app = new Vue({
-        el: '#app'
+        el: '#app',
+        components: {
+            'like-component': likeComponent
+        }
     });
 
 })();
